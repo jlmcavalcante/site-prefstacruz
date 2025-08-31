9a711076fc8b582d4e5ddc327963184b601f1f87
joseluismoreiracavalcante <joseluismoreiracavalcante@gmail.com>
2025-08-31 03:40:58 +0000

    fix: ensure text is black on all pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main className="flex-grow flex flex-col items-center justify-center">
       <section className="text-center px-4 py-8">
-        <h1 className="text-4xl font-bold text-black tracking-tighter mb-4">
+        <h1 className="text-4xl font-bold tracking-tighter mb-4">
           Bem-vindo a Santa Cruz dos Milagres
         </h1>
-        <p className="text-xl mb-8 text-black">
+        <p className="text-xl mb-8">
           A Cidade da Fé e da Peregrinação no Piauí
         </p>
         <div className="flex flex-col sm:flex-row justify-center gap-4">
