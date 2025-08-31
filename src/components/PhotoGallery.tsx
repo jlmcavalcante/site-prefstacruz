25ccae970ee4c6ad499be51d66d8aeb51424aa68
joseluismoreiracavalcante <joseluismoreiracavalcante@gmail.com>
2025-08-31 04:38:38 +0000

    refactor: make photo gallery responsive

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -4,7 +4,7 @@ interface PhotoGalleryProps {
 
 export default function PhotoGallery({ imageUrls }: PhotoGalleryProps) {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {imageUrls.map((url, index) => (
         <div key={index} className="overflow-hidden rounded-lg">
           <img
